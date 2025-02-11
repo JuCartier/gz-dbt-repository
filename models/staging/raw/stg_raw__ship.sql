eb36bb663a990f3dc641f968e23b5a8c353bd552
cartier.julien.pro@gmail.com <cartier.julien.pro@gmail.com>
2025-02-11 11:16:25 +0000

    Add models

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+    orders_id,
+    shipping_fee,
+    shipping_fee_1,
+    logCost,
+    cast(ship_cost as FLOAT64),
+    shipping_fee - shipping_fee_1 as diff
+
+    from source
+
+)
+
+select * from renamed
