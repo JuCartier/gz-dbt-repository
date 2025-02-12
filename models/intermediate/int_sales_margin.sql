350c149d560d61bc62a5df2c6ca3b90482f4b2c9
cartier.julien.pro@gmail.com <cartier.julien.pro@gmail.com>
2025-02-12 10:46:22 +0000

    New folder finance add

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,7 +3,7 @@ SELECT
     date_date,
     revenue,
     quantity,
-    round(quantity * purchase_price,2) as purchase_cost,
-    round(revenue - quantity * purchase_price,2) as margin
+    cast(round(quantity * purchase_price,2) as FLOAT64) as purchase_cost,
+    cast(round(revenue - quantity * purchase_price,2) as FLOAT64) as margin
 FROM {{ ref('stg_raw__sales') }} a
 JOIN {{ ref('stg_raw__product') }} b ON a.products_id = b.products_id
